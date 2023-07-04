ac3aed29b55ef9eadd9b28fe25fe96db23306f35
Henrriky <tectutors.123@gmail.com>
2023-07-04 18:38:25 -0300

    section services

--- src/components/Services/Index.tsx
+++ src/components/Services/Index.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import CardServices from './Card/Index';
-import Foto4 from '../../assets/foto4.png';
+import Lampada from '../../assets/lampada.png';
 import "./index.css"
 
 export default function Services() {
   return (
     <section className="services">
         <h2 className="services__title title">Serviços</h2>
-        <h3 className="about__subtitle subtitle">Conheça alguns dos produtos que nós vendemos</h3>
-        <div className="about__content">
+        <h3 className="services__subtitle subtitle">Conheça alguns dos produtos que nós vendemos</h3>
+        <div className="services__content">
             <CardServices 
                 title="PRODUTOS ELÉTRICOS:" 
                 text="Navegue pela nossa extensa linha de produtos elétricos, incluindo cabos, fios, dispositivos de automação residencial e soluções de iluminação eficientes.Garantimos qualidade e segurança em todas as suas necessidades elétricas.">
-                <img src={Foto4} alt="lampada"/>
+                <img src={Lampada} alt="lampada"/>
             </CardServices>
             <CardServices 
-                title="PRODUTOS ELÉTRICOS:" 
-                text="Navegue pela nossa extensa linha de produtos elétricos, incluindo cabos, fios, dispositivos de automação residencial e soluções de iluminação eficientes.Garantimos qualidade e segurança em todas as suas necessidades elétricas.">
-                <img src={Foto4} alt="lampada"/>
+                title="ACESSÓRIOS GERAIS:" 
+                text="Simplifique sua vida com nossa variedade de acessórios gerais. Encontre itens de organização, segurança e ferramentas úteis para tornar suas tarefas diárias mais práticas e eficientes.">
+                <img src={Lampada} alt="lampada"/>
             </CardServices>
             <CardServices 
-                title="PRODUTOS ELÉTRICOS:" 
-                text="Navegue pela nossa extensa linha de produtos elétricos, incluindo cabos, fios, dispositivos de automação residencial e soluções de iluminação eficientes.Garantimos qualidade e segurança em todas as suas necessidades elétricas.">
-                <img src={Foto4} alt="lampada"/>
+                title="COMBATE À INCÊNDIO:" 
+                text="Priorize a segurança com nossos acessórios especializados de combate a incêndio. De extintores a alarmes e mangueiras, oferecemos soluções confiáveis para proteger você, sua família ou sua empresa contra incêndios.">
+                <img src={Lampada} alt="lampada"/>
             </CardServices>
         </div>
     </section>
